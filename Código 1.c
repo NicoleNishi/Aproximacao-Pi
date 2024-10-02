a13c7027e0122f4709a323a4e68077a476a6bf01
Nicole Nishi <nicole.nishi04@gmail.com>
2024-10-01 22:44:32 -0300

    Arrumei o código C
    Fiz a definição de um valor n e completei a variável y

--- Código 1.c
+++ Código 1.c
@@ -1,25 +1,26 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-double x,y;
-unsigned long long i, j; 
-    
-j= 0;
-    
-srand(time(0));
-    
-for (i=1; i<=n; i++) {
-    
-    x = (double) rand() / RAND\_MAX;
-    
-    y= ;
-    
-    if(x*x + y*y <= 1) {
-    j += 1;
+int main() {
+    double x, y;
+    unsigned long long i, j = 0;
+    unsigned long long n = 1000000;  // Definindo o valor de n
+    double pi_estimation;
+
+    srand(time(0));  // Inicializando a semente para o rand()
+
+    for (i = 1; i <= n; i++) {
+        x = (double) rand() / RAND_MAX;
+        y = (double) rand() / RAND_MAX;
+
+        if (x * x + y * y <= 1) {
+            j += 1;
+        }
     }
+
+    pi_estimation = 4.0 * (double) j / n;  // Calculando a estimativa de pi
+    printf("%1.20f\n", pi_estimation);     // Imprimindo o valor estimado de pi
+
+    return 0;
 }
-    
-    return 4 * (double) j/n; 
-    
-    printf("%1.20f\n", t);
